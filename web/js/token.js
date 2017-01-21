27e136d41a3f6a08bdd87cc967c1bd5085e88723
Konrad <konrad@MacBook-Pro-Konrad.local>
2017-01-21 14:57:45 +0000

    Adding ngCloak for view, adding web3 methods for obtaining blockchain data

--- web/js/token.js
+++ web/js/token.js
@@ -29,23 +29,27 @@ app.controller("mainCtrl", function ($scope, $q) {
         "type": "function"
     }, {"inputs": [], "payable": false, "type": "constructor"}];
 
-    var contract = web3.eth.contract(abi).at(tokenAddress);
+    var contract = undefined;
 
-    function checkBalance(address, callback) {
+    function checkBalance(address) {
+        console.log("Checking balance for " + address);
+        var d = $q.defer();
         contract.balances.call(address, function (error, result) {
             if (!error) {
                 console.log("Balance is " + result)
-                callback(result);
+                d.resolve(result);
             } else {
                 console.error(error);
+                d.reject(error);
             }
-        })
+        });
+        return d.promise;
     }
 
     function issue(amount, callback) {
         contract.issue(amount, {from: web3.eth.accounts[0]}, function (error, result) {
             if (!error) {
-                console.log("Issued currency " + result)
+                console.log("Issued currency " + result);
                 callback(result);
             } else {
                 console.error(error);
@@ -56,7 +60,7 @@ app.controller("mainCtrl", function ($scope, $q) {
     function transfer(recipent, amount, callback) {
         contract.transfer(recipent, amount, {from: web3.eth.accounts[0]}, function (error, result) {
             if (!error) {
-                console.log("Transferred currency " + result)
+                console.log("Transferred currency " + result);
                 callback(result);
             } else {
                 console.error(error);
@@ -68,5 +72,12 @@ app.controller("mainCtrl", function ($scope, $q) {
 
 
     }
+
+    setTimeout(function () {
+        contract = web3.eth.contract(abi).at(tokenAddress);
+        checkBalance(tokenAddress).then(function (result) {
+            self.checkedBalance = result;
+        });
+    }, 1000);
 
 });
