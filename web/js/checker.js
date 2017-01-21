caf1b85c63e1c19ea84952eb8fac8c555f0e0b3a
Konrad <konrad@MacBook-Pro-Konrad.local>
2017-01-21 14:33:59 +0000

    Sending scheletons of controllers

---
+++ web/js/checker.js
@@ -0,0 +1,8 @@
+var app = angular.module("checkerPanelApp", []);
+app.controller("mainCtrl", function ($scope, $q) {
+
+    var self = this;
+
+
+
+});
---
+++ web/js/sender.js
@@ -0,0 +1,8 @@
+var app = angular.module("senderPanelApp", []);
+app.controller("mainCtrl", function ($scope, $q) {
+
+    var self = this;
+
+
+
+});
